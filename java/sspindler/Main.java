709b44b8f55820a7dde599c666fba8a900368f76
Pinto Spindler, Stephan <25225092+s-spindler@users.noreply.github.com>
2022-12-30 22:37:14 +0100

    Make random change to open PR for actions debugging

--- java/sspindler/Main.java
+++ java/sspindler/Main.java
@@ -2,6 +2,6 @@ package sspindler;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is a test");
+        System.out.println("this is another test");
     }
 }
